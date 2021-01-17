cdd525de31b29f63ca5b6fa3def1c4f4a971f8cd
caiquetorres <caiquetorres.bastos@hotmail.com>
2021-01-17 18:52:44 -0300

    feat(globalStyles.ts): added in global styles the "font-size" and the "font-family"

--- src/styles/globalStyles.ts
+++ src/styles/globalStyles.ts
@@ -5,5 +5,8 @@ export const GlobalStyle = createGlobalStyle`
         margin: 0;
         padding: 0;
         box-sizing: border-box;
+
+        font-size: 16px;
+        font-family: Poppins, sans-serif;
     }
 `
